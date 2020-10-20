3f423cd2056b612f3867449a75f11842c0f7fe13
Viacheslav Kulish <kulishviacheslav@gmail.com>
2020-10-20 10:22:46 +0300

    add trust logo

---
+++ example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python prettyqr.py "https://link.trustwallet.com/redeem?code=aTO-E4q-nFi&provider=redeem.trustwallet.com" --logo=example/logo.svg --logo-raster=example/logo.png --color "#3375BB" --output example/output.svg
